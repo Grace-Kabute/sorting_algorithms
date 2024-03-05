fba4e92428c93135ca4dfe479a2bf51c3fee82e0
kabute muthoni <kabutemuthoni@gmail.com>
2024-03-05 11:14:11 -0800

    bubbly

---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in
+ * ascending order using selection sort method
+ * @array - the array to be printed
+ * #size - number of elemets in array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	for i in array[]
+}
