ed2ca2aa605a2fcb1c8a649e5bbb44d4c8c4a859
kabute muthoni <kabutemuthoni@gmail.com>
2024-03-05 17:52:55 -0800

    quick sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,45 @@
 #include "sort.h"
 
 /**
+ * swap_ints - swaps two numbers
+ * @a: one number
+ * @b: another number
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * selection_sort - sorts an array of integers in
  * ascending order using selection sort method
- * @array - the array to be printed
- * #size - number of elemets in array
+ * @array: the array to be printed
+ * @size: number of elemets in array
  */
 
 void selection_sort(int *array, size_t size)
 {
-	for i in array[]
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
 }
