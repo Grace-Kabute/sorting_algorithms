82a626fd298e6cf390ad66df53ea549869918c92
kabute muthoni <kabutemuthoni@gmail.com>
2024-03-11 10:45:55 -0700

    algorithms

--- sort.h
+++ sort.h
@@ -21,6 +21,16 @@ typedef struct listint_s
 } listint_t;
 
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+
 void merge_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
@@ -28,10 +38,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 void quick_sort_hoare(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
-
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
